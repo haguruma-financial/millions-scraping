59065d6731805869927d42bf3fa996c652e4d9ff
kubocker <kubocker@gmail.com>
2020-01-05 21:48:52 +0900

    Develop numbers3 script for inserting to csv

---
+++ numbers3/insert_monthly.py
@@ -0,0 +1,34 @@
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+from datetime import datetime
+import time
+import csv
+
+# 指定
+MONTH = '201912'
+URL = 'https://takarakuji.rakuten.co.jp/backnumber/numbers3/201912/'
+CSV_FILE = '../assets/numbers3/201912.csv'
+
+# アウトプット情報
+output = open(CSV_FILE, mode='w')
+
+# スクレイピング
+scraping = BeautifulSoup(requests.get(URL).content,'html.parser')
+
+# HTMLタグ
+tag_table = scraping.find_all('table')
+# 月間当選番号など
+# テーブル各データ取得
+data = []
+for _i in range(1, len(tag_table)):
+    info = [d.text for d in tag_table[_i].find_all('td')]
+    data.append(info)
+
+# CSVへ挿
+for _j in data:
+    writer = csv.writer(output)
+    writer.writerow(_j)
+
+print('end')
